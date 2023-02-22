dae5c902392bf592a1e0c98627e9e834b28b1371
Ilyahas <info-site@ukr.net>
2023-02-22 10:45:16 +0200

    add viewport timeout

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { View, NativeMethodsMixin, Dimensions, LayoutAnimation } from 'react-nat
 export default class InViewPort extends Component {
   constructor(props) {
     super(props)
-    this.state = { rectTop: 0, rectBottom: 0 }
+    this.state = {rectTop: 0, rectBottom: 0, rectLeft: 0, rectRight: 0}
     this.pendingAnimation = false
   }
 
@@ -25,6 +25,7 @@ export default class InViewPort extends Component {
       this.stopWatching()
     } else {
       this.lastValue = null
+      this.timeoutId = null
       this.startWatching()
     }
   }
@@ -49,7 +50,8 @@ export default class InViewPort extends Component {
           this.setState({
             rectTop: pageY,
             rectBottom: pageY + height,
-            rectWidth: pageX + width
+            rectLeft: pageX,
+            rectRight: pageX + width
           }, () => {
             this.pendingAnimation = false
           })
@@ -66,13 +68,38 @@ export default class InViewPort extends Component {
   isInViewPort() {
     const window = Dimensions.get('window')
     const isVisible =
+      this.state.rectRight != 0 &&
+      this.state.rectLeft >= 0 &&
+      this.state.rectRight <= window.width &&
       this.state.rectBottom != 0 &&
       this.state.rectTop >= 0 &&
-      this.state.rectBottom <= window.height &&
-      this.state.rectWidth > 0 &&
-      this.state.rectWidth <= window.width
+      this.state.rectBottom <= window.height
     if (this.lastValue !== isVisible) {
+      if (isVisible) {
+        if (this.props.timeout) {
+          this.timeoutId = setTimeout(() => {
+            this.isFullyVisible();
+          }, this.props.timeout);
+        } else {
+          this.isFullyVisible();
+        }
+      } else {
+        clearTimeout(this.timeoutId);
+      }
       this.lastValue = isVisible
+    }
+  }
+
+  isFullyVisible() {
+    const window = Dimensions.get('window')
+    const isVisible =
+      this.state.rectRight != 0 &&
+      this.state.rectLeft >= 0 &&
+      this.state.rectRight <= window.width &&
+      this.state.rectBottom != 0 &&
+      this.state.rectTop >= 0 &&
+      this.state.rectBottom <= window.height
+    if (isVisible) {
       this.props.onChange(isVisible)
     }
   }
